8f2b849bc0d1915cc3f8c52393e9e7971ebbcdc8
AlindaB <alindabyamukama@gmail.com>
2022-03-22 13:58:07 +0300

    added a way to check local storage for theme and apply it

--- script.js
+++ script.js
@@ -33,12 +33,23 @@ function switchTheme(event) {
     // console.log(event.target.checked)
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark')
+        localStorage.setItem('theme', 'dark')
         darkMode()
     } else {
         document.documentElement.setAttribute('data-theme', 'light')
+        localStorage.setItem('theme', 'light')
         lightMode()
     }
 }
-
 // Event Listeners
 toggleSwitch.addEventListener('change', switchTheme)
+// Check Local Storage for theme
+const currentTheme = localStorage.getItem('theme')
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme)
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true
+        darkMode()
+    }
+}
+    // console.log(currentTheme)
