c05c044a83d8ca1edb0f7af04f7b29fb5a4e3135
AlindaB <alindabyamukama@gmail.com>
2022-03-22 14:15:49 +0300

    code-review: using ternary operators to make code DRY

--- script.js
+++ script.js
@@ -13,32 +13,25 @@ function imageMode(color) {
     image3.src = `img/undraw_conceptual_idea_${color}.svg`
 }
 // DarkMode Stlyes
-function darkMode() {
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%'
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%'
-    toggleIcon.children[0].textContent = 'Dark Mode'
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
-    imageMode('dark')
+function toggleDarkLightMode(isLight) {
+    nav.style.backgroundColor = isLight ? 'rgb(255 255 255 / 50%' : 'rgb(0 0 0 / 50%'
+    textBox.style.backgroundColor = isLight ? 'rgb(0 0 0 / 50%' : 'rgb(255 255 255 / 50%'
+    toggleIcon.children[0].textContent = isLight ? 'Light Mode' : 'Dark Mode'
+    isLight ?     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun') : toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+    isLight ? imageMode('light') : imageMode('dark')
 }
-// LightMode Styles
-function lightMode() {
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%'
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%'
-    toggleIcon.children[0].textContent = 'Light Mode'
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
-    imageMode('light')
-}
+
 // Switch Theme Dynamically
 function switchTheme(event) {
     // console.log(event.target.checked)
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark')
         localStorage.setItem('theme', 'dark')
-        darkMode()
+        toggleDarkLightMode(false)
     } else {
         document.documentElement.setAttribute('data-theme', 'light')
         localStorage.setItem('theme', 'light')
-        lightMode()
+        toggleDarkLightMode(true)
     }
 }
 // Event Listeners
@@ -49,7 +42,7 @@ if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme)
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true
-        darkMode()
+        toggleLightDarkMode(false)
     }
 }
     // console.log(currentTheme)
